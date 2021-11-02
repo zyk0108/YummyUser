5fe68134c74d047eb7a53b78b8bd1dae25bd8b49
VigilGLC <1063904134@qq.com>
2021-11-02 20:45:17 +0800

    Init.

---
+++ src/main/java/cn/edu/fudan/yummyuser/YummyUserApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.fudan.yummyuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YummyUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YummyUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/edu/fudan/yummyuser/YummyUserApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.fudan.yummyuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YummyUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
