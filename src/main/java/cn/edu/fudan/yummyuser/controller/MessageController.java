6f7aaf79b12043c4a41ca770ae9a00d2dee5c04f
VigilGLC <1063904134@qq.com>
2021-11-07 15:01:44 +0800

    Controller Done.

--- src/main/java/cn/edu/fudan/yummyuser/controller/MessageController.java
+++ src/main/java/cn/edu/fudan/yummyuser/controller/MessageController.java
@@ -1,26 +1,41 @@
 package cn.edu.fudan.yummyuser.controller;
 
+import cn.edu.fudan.yummyuser.domain.Member;
 import cn.edu.fudan.yummyuser.domain.Message;
 import cn.edu.fudan.yummyuser.service.MessageService;
+import cn.edu.fudan.yummyuser.service.SessionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Locale;
 
 
 @RestController
 @RequiredArgsConstructor
 public class MessageController {
     private final MessageService messageService;
+    private final SessionService sessionService;
 
     @PostMapping("/session/{sessionId}/message")
     public ResponseEntity<?> sendMessages(@PathVariable("sessionId") Long sessionId,
                                           @RequestParam("userId") Long userId,
                                           @RequestBody List<Message> messages
     ) {
-        return ResponseEntity.status(HttpStatus.OK).body(null);
+        boolean isMember = false;
+        for (Member mem : sessionService.queryMembers(sessionId)) {
+            if (mem.getUid().equals(userId)) {
+                isMember = true;
+                break;
+            }
+        }
+        if (!isMember) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        messageService.sendMessages(sessionId, userId, messages);
+        return ResponseEntity.ok().body(messages);
     }
 
     @GetMapping("/session/{sessionId}/message")
@@ -28,7 +43,19 @@ public class MessageController {
                                           @RequestParam("lastId") Long lastId,
                                           @RequestParam(value = "order", defaultValue = "asc") String order
     ) {
-        return ResponseEntity.status(HttpStatus.OK).body(null);
+        if (order != null) {
+            order = order.toLowerCase(Locale.ROOT);
+        }
+        if (!"desc".equals(order)) {
+            order = "asc";
+        }
+        final List<Message> messages;
+        if ("asc".equals(order)) {
+            messages = messageService.pollMessagesAsc(sessionId, lastId, 10);
+        } else {
+            messages = messageService.pollMessagesDesc(sessionId, lastId, 10);
+        }
+        return ResponseEntity.ok().body(messages);
     }
 
 
--- src/main/java/cn/edu/fudan/yummyuser/controller/SessionController.java
+++ src/main/java/cn/edu/fudan/yummyuser/controller/SessionController.java
@@ -1,10 +1,14 @@
 package cn.edu.fudan.yummyuser.controller;
 
+import cn.edu.fudan.yummyuser.domain.Session;
+import cn.edu.fudan.yummyuser.domain.SessionState;
 import cn.edu.fudan.yummyuser.service.SessionService;
 import cn.edu.fudan.yummyuser.service.SessionStateService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,23 +18,28 @@ public class SessionController {
 
     @PutMapping("/user/session")
     public ResponseEntity<?> openSession(@RequestParam("uid1") Long uid1, @RequestParam("uid2") Long uid2) {
-        return null;
+        final Session session = sessionService.openSession(uid1, uid2);
+        return ResponseEntity.ok().body(session);
     }
 
-    @DeleteMapping("/user/session/{sessionId}")
-    public ResponseEntity<?> closeSession(@PathVariable("sessionId") Long sessionId) {
-        return null;
+    @DeleteMapping("/user/{userId}/session/{sessionId}")
+    public ResponseEntity<?> closeSession(@PathVariable("userId") Long userId,
+                                          @PathVariable("sessionId") Long sessionId) {
+        final boolean result = sessionStateService.closeState(userId, sessionId);
+        return ResponseEntity.ok().body(result);
     }
 
     @DeleteMapping("/user/{userId}/session/states")
     public ResponseEntity<?> fetchSessionStates(@PathVariable("userId") Long userId) {
-        return null;
+        final List<SessionState> states = sessionStateService.fetchStates(userId);
+        return ResponseEntity.ok(states);
     }
 
     @DeleteMapping("/user/{userId}/session/{sessionId}/states")
     public ResponseEntity<?> readoutSessionState(@PathVariable("userId") Long userId,
                                                  @PathVariable("sessionId") Long sessionId) {
-        return null;
+        sessionStateService.readoutState(userId, sessionId);
+        return ResponseEntity.ok().build();
     }
 
 }
